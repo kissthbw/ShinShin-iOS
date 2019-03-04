83a402c8ebf4a5551a4db02d4197925f30a7a3f4
Juan Osorio <kissthbw@gmail.com>
2019-03-04 17:04:44 -0600

    Add: Proyecto Inicial

---
+++ ShinShin/ViewControllers/UsuarioViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UsuarioViewController.swift
+//  ShinShin
+//
+//  Created by Juan Osorio Alvarez on 3/4/19.
+//  Copyright © 2019 Juan Osorio Alvarez. All rights reserved.
+//
+
+import UIKit
+
+class UsuarioViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
