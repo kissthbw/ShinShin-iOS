45122867a52333f22d1d3bacc18687f13153d400
Juan Osorio <kissthbw@gmail.com>
2019-09-23 17:18:47 -0500

    Add: Clase para controlar el detalle de las fotos a escanear

---
+++ ShinShin/ViewControllers/OCR/DetalleFotosViewController.swift
@@ -0,0 +1,102 @@
+//
+//  DetalleFotosViewController.swift
+//  ShinShin
+//
+//  Created by Juan Osorio Alvarez on 9/18/19.
+//  Copyright © 2019 Juan Osorio Alvarez. All rights reserved.
+//
+
+import UIKit
+
+protocol DetalleFotosViewControllerDelegate: class{
+    func processPhotosViewController(_ controller: DetalleFotosViewController)
+}
+
+class DetalleFotosViewController: UIViewController {
+
+    var photos: [UIImage] = [UIImage]()
+    var imageViews: [UIImageView] = [UIImageView]()
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var btnAgregar: UIButton!
+    @IBOutlet weak var btnListo: UIButton!
+    
+    weak var delegate: DetalleFotosViewControllerDelegate?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        initUI()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    //MARK: - UI Actions
+    @IBAction func back(_ sender: Any) {
+        Model.mantenerCamara = true
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func addPhotoAction(_ sender: Any) {
+        Model.mantenerCamara = true
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func readyAction(_ sender: Any) {
+        //Debe cerrar esta pantalla
+        //LLamar la action de procesar fotos
+        Model.mantenerCamara = true
+        delegate?.processPhotosViewController(self)
+    }
+    
+    
+    //MARK: - Helper methods
+    func initUI(){
+        btnAgregar.layer.cornerRadius = 10.0
+        btnListo.layer.cornerRadius = 10.0
+        let frame = scrollView.frame
+        let total: CGFloat = CGFloat(photos.count)
+        let width = frame.width * total
+        
+        scrollView.delegate = self
+        scrollView.isPagingEnabled = true
+        scrollView.contentSize.width = width + (total * 20)
+        
+        var padding: CGFloat = 0
+        for (index, photo) in photos.enumerated(){
+            print("Foto: \(index)")
+            let tmp = UIImageView()
+            tmp.contentMode = .scaleAspectFill
+            tmp.image = photo
+            let tmpFrame = CGRect(x: (frame.width * CGFloat(index) + padding), y: frame.minY, width: frame.width, height: frame.height)
+            tmp.frame = tmpFrame
+            tmp.layer.cornerRadius = 10.0
+            tmp.clipsToBounds = true
+            padding = 10
+            scrollView.addSubview(tmp)
+        }
+
+        
+//        imagePreview.frame = CGRect(x: imagePreview.frame.width, y: 0, width: imagePreview.frame.width, height: imagePreview.frame.height)
+//        scrollView.addSubview(imagePreview)
+//        scrollView.addSubview(imagePreview)
+    }
+}
+
+//MARK: - Extensions
+extension DetalleFotosViewController: UIScrollViewDelegate{
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        let pageNumber = round(scrollView.contentOffset.x / scrollView.frame.size.width)
+        print("Estas viendo la foto: \(pageNumber)")
+//        pageControl.currentPage = Int(pageNumber)
+    }
+}
